592b2d170b6ccdbb0f570563712a88b2491340fa
Ekin <ccam@ku.edu.tr>
2016-02-25 22:57:36 +0200

    home design

--- client/content/home.js
+++ client/content/home.js
@@ -1,7 +1,36 @@
 $(document).ready(function(){
-$.ajax({url: '/user', cache: false, timeout: 5000, dataType: "json",success: function(data, textStatus, jqXHR) {
-  console.log(data);
-  $("#hpvalue").append(data.HP);
-  $("#energyvalue").append(data.ENERGY);
-}});
+    
+    var _addBtn = $('.add-gadget-btn');
+    var _start = $('#sel1');
+    var _end = $('#sel1');
+    var _gadgetName = $('.gadget-name-inpt');
+    var _currentGadgetArea = $('.currentGadgets');
+    
+    
+    //event listeners
+    _addBtn.on('click', function(e){
+        
+        _currentGadgetArea.append('<div class="gadgetAttrsBox">' + _gadgetName.val() + '</div>');
+    });
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    $.ajax({url: '/user', cache: false, timeout: 5000, dataType: "json",success: function(data, textStatus, jqXHR) {
+    console.log(data);
+    $("#hpvalue").append(data.HP);
+    $("#energyvalue").append(data.ENERGY);
+    }});
+    
+ 
+    
+    
 });
