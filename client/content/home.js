b121d484d9b3bf04013f9b95d33c5fc1d690ac92
Ekin <ccam@ku.edu.tr>
2016-02-28 18:13:50 +0200

    some design

--- client/content/home.js
+++ client/content/home.js
@@ -1,37 +1,89 @@
-$(document).ready(function(){
-    
-    var _addBtn = $('.add-gadget-btn');
-    var _start = $('#sel1');
-    var _end = $('#sel1');
-    var _gadgetName = $('.gadget-name-inpt');
-    var _currentGadgetArea = $('.currentGadgets');
+$(document).ready(function () {
+
+    var _startTime = $('#startDate');
+    var _endTime = $('#endDate');
     var _outlets = $('.outlets');
-    
+
     var _modalTitle = $('#myModalLabel');
-    
-    _outlets.on('click', function(e){
+
+    _outlets.on('click', function (e) {
         _modalTitle.innerHTML = 'soket' + $.data(e.target, 'soket');
-        console.log($.data(e.target, 'soket') );
-       // $( this ).css( "stroke", "red" );
+        console.log($.data(e.target, 'soket'));
+        // $( this ).css( "stroke", "red" );
     });
-    
-        
-        $.each(_outlets, function(i, v){
-            $.data(v, 'soket', i);
-            
-        });
-        
-        
-        $('#myModal').on('show.bs.modal', function (event) {
-            
-            var currentButton = event.relatedTarget; // Button that triggered the modal
-            var recipient = $.data(currentButton, 'soket'); // Extract info from data-* attributes
-            // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-            // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-            var modal = $(this);
-            modal.find('.modal-title').text('New message to soket no = ' + recipient);
-            //modal.find('.modal-body input').val(recipient)
-        });
+
+
+    $.each(_outlets, function (i, v) {
+        $.data(v, 'soket', i);
+
+    });
+
+
+    $('#myModal').on('show.bs.modal', function (event) {
+
+        var currentButton = event.relatedTarget; // Button that triggered the modal
+        var recipient = $.data(currentButton, 'soket');
+
+        var modal = $(this);
+        modal.find('.modal-title').text('New message to soket no = ' + recipient);
+        _startTime.val('11:31');
+        _endTime.val('11:32');
+        //modal.find('.modal-body input').val(recipient)
+    });
+
+
+    $('.clockpicker').clockpicker({
+        donetext: 'Done',
+        init: function () {
+            console.log("colorpicker initiated");
+        },
+        beforeShow: function () {
+            console.log("before show");
+        },
+        afterShow: function () {
+            console.log("after show");
+        },
+        beforeHide: function () {
+            console.log("before hide");
+        },
+        afterHide: function () {
+            console.log("after hide");
+        },
+        beforeHourSelect: function () {
+            console.log("before hour selected");
+        },
+        afterHourSelect: function () {
+            console.log("after hour selected");
+        },
+        beforeDone: function () {
+            console.log("before done");
+        },
+        afterDone: function () {
+            console.log("after done");
+        }
+    });
+                
+                
+                
+    //****weekday
+    var checkboxes = $(':checkbox'),
+        span = $('#allChecked');
+
+    checkboxes.prop('checked', true);
+
+    checkboxes.on('change', function () {
+        var checked = checkboxes.filter(':checked');
+        if (checked.length == checkboxes.length)
+            span.text('(All Days Selected)');
+        else {
+            var days = jQuery.map(checked, function (n, i) { return n.id; });
+            span.text('(' + days.join(', ') + ' Selected)');
+        }
+    });
+       
+       
+       
+    //
 });
 
 
